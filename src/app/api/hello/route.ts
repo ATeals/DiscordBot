1988aed38d4f74f160f3fd489d97525ae5d5386a
ateals <ateals@icloud.com>
2024-01-04 17:27:31 +0900

    feat: Post route

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest) => {
+export const POST = async (req: NextRequest) => {
   const res = await axios.post("https://discord.com/api/webhooks/" + process.env.TEST_CHEANEL, {
     content: "oh! github webhook is coming!!",
   });
