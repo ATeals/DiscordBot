ff2e12dd01ada1982565cfdc5773209635e1c4f6
ateals <ateals@icloud.com>
2024-01-04 17:00:23 +0900

    feat: new discord bot

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,10 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
-}
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  const res = await axios.post("https://discord.com/api/webhooks/" + process.env.TEST_CHEANEL, {
+    content: "oh! github webhook is coming!!",
+  });
+
+  return NextResponse.json({ massage: "hello user!" });
+};
